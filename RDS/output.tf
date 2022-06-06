3cdfa345ceb2c25d10db4e726e69748ebdad442e
Khrystyna <krybitska@gmail.com>
2022-06-06 03:34:15 +0000

    added output

---
+++ RDS/output.tf
@@ -0,0 +1,16 @@
+output address {
+  value = "aws_rds.ilearning.address"
+}
+output availability_zone {
+  value = "aws_rds.ilearning.availability_zone"
+}
+output engine {
+  value = "aws_rds.ilearning.engine"
+}
+output id {
+  value = "aws_rds.ilearning.id"
+}
+
+output port {
+  value = "aws_rds.ilearning.port" 
+}
