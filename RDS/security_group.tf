456e6b5bdabc7aae89138f8297d32ec964d10e5c
Khrystyna <krybitska@gmail.com>
2022-06-06 03:38:56 +0000

    added groups

---
+++ RDS/security_group.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "db" {
+  name_prefix        = "db"
+  description = "Terraform example RDS MySQL server"
+  # Keep the instance private by only allowing traffic from the web server.
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+  }
+  # Allow all outbound traffic.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = local.task_tags
+}
